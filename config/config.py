200ced2cb9e6d3f9266b19999247377808b1c55b
rachelzhaolp <rachelzhaolp@gmail.com>
2020-06-08 01:08:58 -0500

    add config

--- config/config.py
+++ config/config.py
@@ -1,12 +1,33 @@
 import os
 from os import path
+PROJECT_HOME = path.dirname(path.abspath(__file__))
+
 
 """
-Logging config
+config for default path arguments in run.py
 """
-# Getting the parent directory of this file. That will function as the project home.
-PROJECT_HOME = path.dirname(path.abspath(__file__))
-LOGGING_CONFIG = path.join(PROJECT_HOME, 'logging/logging.conf')
+# config
+CONFIG_YAML = "config/config.yaml"
+# raw data
+RAW_DATA = "data/online_retail_II.csv"
+OBJECT_NAME = "online_retail_II.csv"
+BUCKET_NAME = "msia423-product-recommendation"
+# clean data
+CLEAN_DATA = "data/clean_data.csv"
+# basket
+BASKETS = "data/basket.csv"
+# product_dim
+PRODUCT_DIM = "data/product_dim.csv"
+# train/test
+TRAIN_DATA = "data/training_data.csv"
+TEST_DATA = "data/test_data.csv"
+# recommendations
+REC_PATH = "data/rec.csv"
+# evaluation
+AUC_ACCURACY_PATH = "evaluations/accuracy.csv"
+# eda
+FIGURE_PATH = "figures/"
+
 
 """
 RDS
@@ -15,36 +36,19 @@ RDS
 CONN_TYPE = "mysql+pymysql"
 USER = os.environ.get("MYSQL_USER")
 PASSWORD = os.environ.get("MYSQL_PASSWORD")
-HOST = os.environ.get("MYSQL_HOST")
-PORT = os.environ.get("MYSQL_PORT")
+MYSQL_HOST = os.environ.get("MYSQL_HOST")
+MYSQL_PORT = os.environ.get("MYSQL_PORT")
 DATABASE = os.environ.get("DATABASE_NAME")
-RDS_ENGING_STRING = "{}://{}:{}@{}:{}/{}".format(CONN_TYPE, USER, PASSWORD, HOST, PORT, DATABASE)
+RDS_ENGING_STRING = "{}://{}:{}@{}:{}/{}".format(CONN_TYPE, USER, PASSWORD, MYSQL_HOST, MYSQL_PORT, DATABASE)
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+TABLE = 'prods_recs'
 
 """
 Local database connection config
 """
+
 LOCAL_DATABASE = "market_basket_analysis.db"
 DATABASE_PATH = path.join(path.dirname(PROJECT_HOME), 'data/{}'.format(LOCAL_DATABASE))
 LOCAL_ENGING_STRING = 'sqlite:////{}'.format(DATABASE_PATH)
-
-
-
-"""
-Local file config - which csv file to be uploaded to RDS/Local database
-"""
-# pseudo file, change later
-
-PRED_FILE = "pred.csv"
-TABLE = "prds_rec"
-
-"""
-S3
-"""
-
-OBJECT_NAME = "online_retail_II.csv"
-BUCKET_NAME = "msia423-product-recommendation"
-FILE_NAME = "online_retail_II.csv"
-
-
-
--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,9 +1,18 @@
-DEBUG = True
-LOGGING_CONFIG = "config/logging/local.conf"
-PORT = 5000
-APP_NAME = "penny-lane"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+import os
+import config.config as config
+
+DEBUG = True  # Keep True for debugging, change to False when moving to production
+HOST = "0.0.0.0"  # the host that is running the app. 0.0.0.0 when running locally
+PORT = 5000  # What port to expose app on. Must be the same as the port exposed in app/Dockerfile
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'SQLALCHEMY_DATABASE_URI')  # URI (engine string) for database that contains tracks
+APP_NAME = "Rachel's Grocery Store"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
-MAX_ROWS_SHOW = 100
+
+if SQLALCHEMY_DATABASE_URI is not None:
+    pass
+elif config.MYSQL_HOST is None:
+    SQLALCHEMY_DATABASE_URI = config.LOCAL_ENGING_STRING
+else:
+    SQLALCHEMY_DATABASE_URI = config.RDS_ENGING_STRING
