73cc6a8171b40ffe86c54e088aaa474c58af255a
rachelzhaolp <rachelzhaolp@gmail.com>
2020-06-10 00:46:01 -0500

    change try except

--- src/train.py
+++ src/train.py
@@ -5,11 +5,93 @@ import config.config as conf
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 from src.helper import read_csv, save_csv, load_config
+import numpy as np
 
 pd.options.mode.chained_assignment = None
 
 logger = logging.getLogger(__name__)
 project_path = path.dirname(path.dirname(path.abspath(__file__)))
+
+
+def main(args):
+    """
+    main function to run the market basket analysis and save the recommendations to csv
+    :param args: (argparse) user-input configuration file
+    """
+    try:
+        config_path = project_path + "/" + args.config
+        input_data_path = project_path + "/" + args.input
+        output_data_path = project_path + "/" + args.output
+        product_path = project_path + "/" + conf.PRODUCT_DIM
+
+        config = load_config(config_path)
+        df = read_csv(input_data_path)
+        product = read_csv(product_path)
+
+        result = train(df, **config['train'])
+
+        # Join product object table to get the name and price.
+
+        final_results = join_info(result, product, "StockCode", "StockCode")
+        final_results = join_info(final_results, product, "rec1", "StockCode")
+        final_results = join_info(final_results, product, "rec2", "StockCode")
+
+        # format conf
+        final_results['conf1'] = round(final_results['conf1'] * 100, 2)
+        final_results['conf2'] = round(final_results['conf2'] * 100, 2)
+
+        final_results = final_results[config["result_columns"]]
+
+        # Write to output file
+        save_csv(final_results, output_data_path)
+    except KeyError as e3:
+        logger.error("KeyError: " + str(e3))
+    except ValueError as e4:
+        logger.error("ValueError: " + str(e4) + " Please validate Values in the configuration file.")
+    except Exception as e:
+        logger.error("Unexpected error occurred when making recommendations: " + str(e))
+
+
+def train(df, apriori_args, association_rules_args, ante_num_filter, rank_arg, rank_filter, to_columns):
+    """
+    Run market basket analysis on baskets data, return recommendations
+    Args:
+        df: (DataFrame) Baskets
+        apriori_args: (Dictionary) Arguments of apriori()
+        association_rules_args: (Dictionary) Arguments of the association_rules()
+        ante_num_filter: (Integer) Threshold of the maximum number of items in antecedents
+        rank_filter: (Integer) Threshold of number recommendations for each item
+        rank_arg: (Dictionary) Argument of the rank() function
+        to_columns: (List of String) Column names of the new DataFrame
+
+    Returns: (DataFrame) Recommendations
+
+    """
+
+    df = df.set_index('Invoice')
+    # all products
+    prods = df.columns.to_frame()
+
+    # Calculate support for different baskets
+    frequent_itemsets = get_support(df, apriori_args)
+    popular_list = get_popular(frequent_itemsets)
+
+    # Calculate confidence
+    rules = get_recommendations(frequent_itemsets, association_rules_args, ante_num_filter, rank_filter, rank_arg)
+
+    # Change data types of antecedents and consequents to string
+    rules = frozenset2String(rules, 'antecedents')
+    rules = frozenset2String(rules, 'consequents')
+
+    # Pivot the DataFrame
+    rec = pivot_rules(rules, to_columns)
+    # Get full table
+    rec = get_full_table(prods, rec, to_columns)
+
+    # Fill na
+    rec = fill_no_rec(rec, popular_list)
+    logger.info("Successfully made recommendations for {} products".format(len(rec)))
+    return rec
 
 
 def get_support(df, apriori_args):
@@ -22,17 +104,15 @@ def get_support(df, apriori_args):
     Returns: (DataFrame)frequent_itemsets
 
     """
-    try:
-        logger.info("Calculating supports for each basket")
-        frequent_itemsets = apriori(df, **apriori_args)
-        logger.info("Successfully calculated supports")
+    logger.warning("Calculate supports for item sets is heavy on memory, please increase the memory limitation to at "
+                   "least 12GB")
+    logger.info("Calculating supports for the item sets")
+    frequent_itemsets = apriori(df, **apriori_args)
+    logger.info("Successfully calculated supports")
+    if len(frequent_itemsets) < 2:
+        raise ValueError("No items set meet the limit of minimum support, please use a smaller min_support.")
+    else:
         return frequent_itemsets
-    except KeyError as e3:
-        logger.error("KeyError: " + str(e3) + " Please validate Keys in the configuration file.")
-        raise KeyError
-    except ValueError as e4:
-        logger.error("ValueError: " + str(e4) + " Please validate Values in the configuration file.")
-        raise ValueError
 
 
 def get_popular(frequent_itemsets):
@@ -63,9 +143,12 @@ def get_recommendations(frequent_itemsets, association_rules_args, ante_num_filt
     Returns:(DataFrame) association_rules
 
     """
-    try:
-        rules = association_rules(frequent_itemsets, **association_rules_args)
-
+    logger.info("Calculating confidence for association_rules")
+    rules = association_rules(frequent_itemsets, **association_rules_args)
+    if len(rules) == 0:
+        raise ValueError("No rules meet the limit, please try a smaller min_support in get_support() or a samller "
+                         "min_threshold ")
+    else:
         # filter recommendations
         rules['ante_num'] = rules['antecedents'].apply(len)
         rules = rules[rules['ante_num'] <= ante_num_filter]
@@ -73,12 +156,6 @@ def get_recommendations(frequent_itemsets, association_rules_args, ante_num_filt
         rules = rules[rules['rank'] <= rank_filter]
         logger.info("Successfully calculated confidences")
         return rules
-    except KeyError as e3:
-        logger.error("KeyError: " + str(e3) + " Please validate Keys in the configuration file.")
-        raise KeyError
-    except ValueError as e4:
-        logger.error("ValueError: " + str(e4) + " Please validate Values in the configuration file.")
-        raise ValueError
 
 
 def frozenset2String(rules, colname):
@@ -91,15 +168,8 @@ def frozenset2String(rules, colname):
     Returns:(DataFrame)
 
     """
-    try:
-        rules.loc[:, colname] = [','.join(map(str, prod)) for prod in rules[colname]]
-        return rules
-    except KeyError as e3:
-        logger.error("KeyError: " + str(e3) + " Please validate Keys in the configuration file.")
-        raise KeyError
-    except ValueError as e4:
-        logger.error("ValueError: " + str(e4) + " Please validate Values in the configuration file.")
-        raise ValueError
+    rules.loc[:, colname] = [','.join(map(str, prod)) for prod in rules[colname]]
+    return rules
 
 
 def pivot_rules(rules, to_columns):
@@ -112,14 +182,15 @@ def pivot_rules(rules, to_columns):
     Returns:(DataFrame)
 
     """
-    try:
-        rec = rules.pivot(index='antecedents', columns='rank', values=['consequents', 'confidence'])
+    rec = rules.pivot(index='antecedents', columns='rank', values=['consequents', 'confidence'])
+    if rec.shape[1] == 2:
+        rec['rec2'] = np.nan
+        rec['conf2'] = np.nan
+        rec.columns = ['rec1', 'conf1', 'rec2', 'conf2']
+    else:
         rec.columns = to_columns[1:]
-        rec = rec.reset_index()
-        return rec
-    except KeyError as e3:
-        logger.error("KeyError: " + str(e3) + " Please validate Keys in the configuration file.")
-        raise KeyError
+    rec = rec.reset_index()
+    return rec
 
 
 def get_full_table(prods, rec, to_columns):
@@ -171,97 +242,7 @@ def join_info(df, info_df, lefton, righton):
     Returns:(DataFrame)
 
     """
-    try:
-        to_df = df.set_index(lefton).join(info_df.set_index(righton), lsuffix='', rsuffix='_' + lefton,
-                                          how="inner").reset_index()
-        to_df = to_df.rename(columns={'index': lefton})
-        return to_df
-    except KeyError as e3:
-        logger.error("KeyError: " + str(e3) + " Please validate Keys in the configuration file.")
-        raise KeyError
-
-
-def train(df, apriori_args, association_rules_args, ante_num_filter, rank_arg, rank_filter, to_columns):
-    """
-    Run market basket analysis on baskets data, return recommendations
-    Args:
-        df: (DataFrame) Baskets
-        apriori_args: (Dictionary) Arguments of apriori()
-        association_rules_args: (Dictionary) Arguments of the association_rules()
-        ante_num_filter: (Integer) Threshold of the maximum number of items in antecedents
-        rank_filter: (Integer) Threshold of number recommendations for each item
-        rank_arg: (Dictionary) Argument of the rank() function
-        to_columns: (List of String) Column names of the new DataFrame
-
-    Returns: (DataFrame) Recommendations
-
-    """
-
-    try:
-        df = df.set_index('Invoice')
-        # all products
-        prods = df.columns.to_frame()
-
-        # Calculate support for different baskets
-        frequent_itemsets = get_support(df, apriori_args)
-        popular_list = get_popular(frequent_itemsets)
-
-        # Calculate confidence
-        rules = get_recommendations(frequent_itemsets, association_rules_args, ante_num_filter, rank_filter, rank_arg)
-
-        # Change data types of antecedents and consequents to string
-        rules = frozenset2String(rules, 'antecedents')
-        rules = frozenset2String(rules, 'consequents')
-
-        # Pivot the DataFrame
-        rec = pivot_rules(rules, to_columns)
-        # Get full table
-        rec = get_full_table(prods, rec, to_columns)
-
-        # Fill na
-        rec = fill_no_rec(rec, popular_list)
-        logger.info("Successfully made recommendations for {} products".format(len(rec)))
-        return rec
-
-    except KeyError as e3:
-        logger.error("KeyError: " + str(e3) + " Please validate Keys in the configuration file.")
-    except ValueError as e4:
-        logger.error("ValueError: " + str(e4) + " Please validate Values in the configuration file.")
-    except Exception as e:
-        logger.error("Unexpected error occurred when making recommendations: " + str(e))
-
-
-def main(args):
-    """
-    main function to clean data
-    :param args: (argparse) user-input configuration file
-    """
-    try:
-        config_path = project_path + "/" + args.config
-        input_data_path = project_path + "/" + args.input
-        output_data_path = project_path + "/" + args.output
-        product_path = project_path + "/" + conf.PRODUCT_DIM
-
-        config = load_config(config_path)
-        df = read_csv(input_data_path)
-        product = read_csv(product_path)
-
-        result = train(df, **config['train'])
-
-        # Join product object table to get the name and price.
-
-        final_results = join_info(result, product, "StockCode", "StockCode")
-        final_results = join_info(final_results, product, "rec1", "StockCode")
-        final_results = join_info(final_results, product, "rec2", "StockCode")
-
-        # format conf
-        final_results['conf1'] = round(final_results['conf1'] * 100, 2)
-        final_results['conf2'] = round(final_results['conf2'] * 100, 2)
-
-        final_results = final_results[config["result_columns"]]
-
-        # Write to output file
-        save_csv(final_results, output_data_path)
-
-    except Exception as e:
-        logger.error("Unexpected error occurred when making recommendations: " + str(e))
+    to_df = df.set_index(lefton).join(info_df.set_index(righton), lsuffix='', rsuffix='_' + lefton,
+                                      how="inner").reset_index()
+    to_df = to_df.rename(columns={'index': lefton})
+    return to_df
