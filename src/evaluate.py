90805f0e5042b5df398c52767105332bcac87b62
rachelzhaolp <rachelzhaolp@gmail.com>
2020-06-10 00:44:12 -0500

    change try except

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,10 +1,30 @@
 import logging
 import pandas as pd
 from os import path
-from src.helper import read_csv, save_csv, load_config, check_num
+from src.helper import read_csv, save_csv
 
 logger = logging.getLogger(__name__)
 project_path = path.dirname(path.dirname(path.abspath(__file__)))
+
+
+def main(args):
+    """
+    main function to clean data
+    :param args: (argparse) user-input configuration file
+    """
+    try:
+        rec_path = project_path + "/" + args.rec
+        test_data_path = project_path + "/" + args.test
+        output_data_path = project_path + "/" + args.output
+
+        rec = read_csv(rec_path)
+        test = read_csv(test_data_path)
+
+        accuracy = accuracy_calculator(rec, test)
+        # Write to output file
+        save_csv(accuracy, output_data_path)
+    except Exception as e:
+        logger.error("Unexpected error occurred when evaluation: " + str(e))
 
 
 def accuracy_calculator(rec, test):
@@ -32,23 +52,3 @@ def accuracy_calculator(rec, test):
     out['rec2_accuracy'] = out['num_with_rec2'] / out['num_stock']
     out['overall_accuracy'] = (out['rec1_accuracy'] + out['rec2_accuracy']) / 2
     return out[['rec1_accuracy', 'rec2_accuracy', 'overall_accuracy']].agg(['mean', 'max']).round(2)
-
-
-def main(args):
-    """
-    main function to clean data
-    :param args: (argparse) user-input configuration file
-    """
-    try:
-        rec_path = project_path + "/" + args.rec
-        test_data_path = project_path + "/" + args.test
-        output_data_path = project_path + "/" + args.output
-
-        rec = read_csv(rec_path)
-        test = read_csv(test_data_path)
-
-        accuracy = accuracy_calculator(rec, test)
-        # Write to output file
-        save_csv(accuracy, output_data_path)
-    except Exception as e:
-        logger.error("Unexpected error occurred when evaluation: " + str(e))
